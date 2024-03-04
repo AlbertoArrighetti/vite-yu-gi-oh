7e55d039a7eb70bef841af4e1a9cf0e3a6882b34
Alberto-Arrighetti <alberto.arrighetti1571@gmail.com>
2024-03-04 17:24:05 +0100

    found card counter

--- src/components/CardList.vue
+++ src/components/CardList.vue
@@ -19,29 +19,48 @@ export default {
 </script>
 
 <template>
-<div class="ext_container">
-    <ul>
-        <CardItem 
-        v-for="currentCard in store.cards"
-        :card="currentCard"
-        ></CardItem>
+    <main>
+        <div class="ext_container">
         
-    </ul>
-</div>
+            
+            <div class="counter">
+                Found {{ store.cards.length }} cards
+            </div>
+            <ul>
+                <CardItem 
+                v-for="currentCard in store.cards"
+                :card="currentCard"
+                ></CardItem>
+                
+            </ul>
+        </div>
+    </main>
 
 </template>
 
 <style lang="scss">
 @use '../styles/variables' as *;
 
-ul {
-    list-style-type: none;
-
-    display: flex;
-    flex-flow: row wrap;
-    
-    gap:24px $cardListGap;
+.ext_container {
     padding: 70px;
+    margin-bottom: 50px;
+
+    .counter {
+        padding: 28px 24px;
+
+        font-weight: bold;
+        color: white;
+        background-color: #212529;
+    }
+
+    ul {
+        list-style-type: none;
+    
+        display: flex;
+        flex-flow: row wrap;
+        
+        gap:24px $cardListGap;
+    }
 }
 
 </style>
