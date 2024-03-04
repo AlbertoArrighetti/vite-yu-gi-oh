a7e357028db2422b720cd140ebd8c4cce3d03c37
Alberto-Arrighetti <alberto.arrighetti1571@gmail.com>
2024-03-04 15:28:34 +0100

    axios cards imported

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,34 @@
 <script>
+// import axios
+import axios from 'axios';
+// store
+import { store } from './store.js';
 
+export default {
+  data() {
+    return {
+      store,
+    }
+  },
+
+  created() {
+
+    axios
+      .get('https://db.ygoprodeck.com/api/v7/cardinfo.php?num=20&offset=0')
+      .then(res => {
+        console.log(res.data.data)
+      })
+    
+  },
+  components: {
+  
+  },
+
+}
 </script>
 
 <template>
-  <h1>ciao</h1>
+  
 </template>
 
 <style lang="scss">
